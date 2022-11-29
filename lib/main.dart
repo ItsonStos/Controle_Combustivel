686fdec5d24e297d6ce174c20e9f49959489568e
Itson Stos <84933405+ItsonStos@users.noreply.github.com>
2022-11-29 08:30:45 -0300

    Update main.dart
    atualização do arquivo main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:controle_combustivel/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:supply_control/bottomNavigationBar.dart';
 
-void main() => runApp(MaterialApp(
-      home: BottomNavigation(),
+void main() =>
+  runApp(
+    // ignore: prefer_const_constructors
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-    ));
+      //theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    )
+  );
+
+
+
